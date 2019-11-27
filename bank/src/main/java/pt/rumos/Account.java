eb88de7980b5f84c0dfadf882e6814e095c5ef7d
JorgeCardoso9 <jorgemcardoso924@gmail.com>
2019-11-27 21:39:07 +0000

    some logic

--- bank/src/main/java/pt/rumos/Account.java
+++ bank/src/main/java/pt/rumos/Account.java
@@ -1,10 +1,38 @@
 package pt.rumos;
 
 public class Account {
-    private int accountNum;
-    private String accountName;
+
     private double balance;
+    private String name;
+    private long acctNum;
+
+    //Constructor -- initializes balance, owner, and account number
+    public Account(double initBal, String owner, long number) {
+        balance = initBal;
+        name = owner;
+        acctNum = number;
+    }
 
 
+    public void withdraw(double amount) {
+        if (balance >= amount)
+            balance -= amount;
+        else
+            System.out.println("Insufficient funds");
+    }
 
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String toString() {
+        return "Name:" + name +
+                "\nAccount Number: " + acctNum +
+                "\nBalance: " + balance;
+
+    }
 }
--- bank/src/main/java/pt/rumos/Main.java
+++ bank/src/main/java/pt/rumos/Main.java
@@ -5,5 +5,15 @@ public class Main
 {
     public static void main( String[] args ){
 
+        Account acc1 = new Account(500, "jorge", 1 );
+        Account acc2 = new Account(1000, "arthur", 2);
+
+        System.out.println("Jorge balance before: " + acc1.getBalance());
+        System.out.println("Arthur balance before: " + acc2.getBalance());
+
+        acc1.withdraw(100);
+
+        System.out.println(acc1.getBalance());
+
     }
 }
